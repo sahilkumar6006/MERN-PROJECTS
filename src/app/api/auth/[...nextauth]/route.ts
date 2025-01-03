f4a4171d7352df6128ecd36e5af2cdbb7cf58a11
M2 <sahil.kumar@geeky.dev>
2025-01-03 09:46:09 +0530

    added callback in thee route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,17 @@ const handler = NextAuth({
           clientId: process.env.GOOGLE_CLIENT_ID ?? "",
           clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
-      ]
+      ],
+      callbacks: {
+        async signIn({user, account, profile, email, credentials}{
+          if(account?.provider === "google"){
+            const email = user.email;
+            if(!email){
+              return false;
+            }
+        })
+        return false
+      }
 })
 
 
