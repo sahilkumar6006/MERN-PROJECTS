fd5622538241d34536de52f2187659b305d96156
M2 <sahil.kumar@geeky.dev>
2025-01-07 09:46:14 +0530

    changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,50 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { Keypair } from "@solana/web3.js";
+import prisma from "@/lib/prisma"; 
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        })
-      ],
-      callbacks: {
-        async signIn({user, account, profile, email, credentials}{
-          if(account?.provider === "google"){
-            const email = user.email;
-            if(!email){
-              return false;
-            }
-        })
-        return false
-      }
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "google") {
+        const email = user.email;
 
+        if (!email) {
+          return false; // Reject sign-in if email is missing
+        }
 
-export {handler as GET, handler as POST}
+        // Check if the user already exists in the database
+        const userDb = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        if (userDb) {
+          return false; // Reject sign-in if user already exists
+        }
+
+       
+        const keypair = Keypair.generate();
+        const publicKey = keypair.publicKey.toBase58();
+
+        
+        await prisma.user.create({
+          data: {
+            email,
+            publicKey, 
+          },
+        });
+
+        return true; 
+
+      return false; 
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
