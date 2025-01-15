e3b94f5f0d3a361dbf15a6a372b2971bf8cbc5a1
M2 <sahil.kumar@geeky.dev>
2025-01-15 21:45:06 +0530

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
         <Hero/>
-        <div>
-          hello
-        </div>
+        
       </main>
   );
 }
